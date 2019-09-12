509cc5f004b82cd2223c7d6d4c0c4dadb2f95919
lenovo <lenovo@LAPTOP-6S83ABE6>
2019-09-12 14:50:12 +0700

    Add Test Case 11 and Test Suite Regression

--- Test Suites/Regression Test/Regression Test.ts
+++ Test Suites/Regression Test/Regression Test.ts
@@ -22,4 +22,64 @@
       <isRun>true</isRun>
       <testCaseId>Test Cases/Transaction/TC-2</testCaseId>
    </testCaseLink>
+   <testCaseLink>
+      <guid>2c4624ab-81c3-46b1-bed8-85a9b8571cfa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Transaction/TC-10</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3bc460af-663c-4c33-a5f8-e9eeb2a45a72</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Transaction/TC-11</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9c657485-e274-4803-a1d9-4932489a306a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Transaction/TC-2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d9c12da0-ef31-4413-bf2b-5c8f2d403387</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Transaction/TC-3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c7e871f3-befd-4a97-b4b8-e69fca6e42a1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Transaction/TC-4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6c80be4f-c55b-4620-8e84-d53fd0d1564b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Transaction/TC-5</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2b5f6c0b-ff2d-4aab-925d-648022960d8b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Transaction/TC-6</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d87774b5-4f15-4d02-bff7-95a70288b8a7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Transaction/TC-7</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d7c12d88-6581-429d-b38f-e689271f18b6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Transaction/TC-8</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c53c9325-59c5-473e-8f63-8f33b84b876e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Transaction/TC-9</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
