1b82114301cf7455785a53ae8ee0013898d4bbad
lenovo <lenovo@LAPTOP-6S83ABE6>
2019-09-12 14:42:55 +0700

    add test case and test suite

---
+++ Test Suites/Regression Test/Regression Test.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>67182adf-1a88-4ee2-9b18-4c5b2d64e76a</testSuiteGuid>
+   <testCaseLink>
+      <guid>ed70c8b3-f0b8-4a82-b216-101ac9d17a7f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>62a0b583-2efb-455c-a1be-0fa45871d6ba</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Smoke Test/Smoke Test.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ee0b6c5e-ea9e-480f-8530-54179da5649a</testSuiteGuid>
+   <testCaseLink>
+      <guid>c1f57941-98a4-497a-8ac4-1f759fb87603</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-3</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
